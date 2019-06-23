d5d623a2e0457d99b0f5c5c41474408eb09dd2da
chanakaDe <chanu1993@gmail.com>
2019-06-24 00:30:09 +0200

    Added new route to generate session dates and repeated event dates. Fixed date list generation issue and added weekly and monthly session generation capability.

--- server/routes/session.js
+++ server/routes/session.js
@@ -1,5 +1,6 @@
 import express from 'express';
 var moment = require('moment');
+require('moment-weekday-calc');
 
 let router = express.Router();
 
@@ -15,20 +16,22 @@ let router = express.Router();
 router.get('/generate', (req, res, next) => {
 
     let startDate = req.query.startDate;
-    let period = req.query.period;
+    let finalStartDate = req.query.startDate; // This variable is used to generate dates list.
+    let period = req.query.period; // How many dates or weeks or months
     let durationType = req.query.durationType; //Days , Weeks , Months
-    let dayLimit = req.query.dayLimit; // 20 , 25 , 30
-    let finalEndDate = "";
+    let dayLimit = req.query.dayLimit; // 20 = without weekened , 25 = weekdays + Saturday , 30 = Weekdays + Saturday + Sunday
+    let finalEndDate = ""; // auto generated value from below functions.
 
     console.log("start date : ", startDate);
     console.log("period : ", period);
     console.log("durationType : ", durationType);
     console.log("dayLimit : ", dayLimit);
 
+    // Final date ( End Date ) generation for Days
     if (durationType == "Days") {
         console.log("Days");
 
-        // 20 days system
+        // 20 days system ( without weekend )
         if (dayLimit == "20") {
             console.log("20 days");
             startDate = new Date(startDate.replace(/-/g, "/"));
@@ -43,7 +46,7 @@ router.get('/generate', (req, res, next) => {
             finalEndDate = moment(endDate).format('YYYY-MM-DD');
         }
 
-        // 25 days system
+        // 25 days system ( Weekdays + Saturday )
         if (dayLimit == "25") {
 
             startDate = new Date(startDate.replace(/-/g, "/"));
@@ -58,7 +61,7 @@ router.get('/generate', (req, res, next) => {
             finalEndDate = moment(endDate).format('YYYY-MM-DD');
         }
 
-        // 30 days system
+        // 30 days system ( Weekdays + Saturday + Sunday)
         if (dayLimit == "30") {
 
             startDate = new Date(startDate.replace(/-/g, "/"));
@@ -74,7 +77,7 @@ router.get('/generate', (req, res, next) => {
         }
     }
 
-    // Weeks
+    // Final date ( End Date ) generation for Weeks
     if (durationType == "Weeks") {
         console.log("Weeks");
         var updatedDate = moment(startDate, "YYYY-MM-DD").add('weeks', period - 1);
@@ -87,14 +90,9 @@ router.get('/generate', (req, res, next) => {
         finalEndDate = end_date;
     }
 
-    // Months
+    // Final date ( End Date ) generation for Months
     if (durationType == "Months") {
         console.log("Months");
-        // if (loan_period == 1) {
-        //     new_date = moment(startdate, "YYYY-MM-DD").add('months', loan_period);
-        // } else {
-        //     new_date = moment(startdate, "YYYY-MM-DD").add('months', loan_period);
-        // }
 
         var updatedDateMonth = moment(startDate, "YYYY-MM-DD").add('months', period);
 
@@ -111,19 +109,22 @@ router.get('/generate', (req, res, next) => {
     var datesList = [];
     var duration = period;
 
-    // Daily Option
+    // Daily Option, Calculate dates list with start date and end date.
     if (durationType == "Days") {
 
         if (dayLimit == 20) {
-
+            console.log("*******")
+            console.log(finalStartDate)
+            console.log(finalEndDate)
+            console.log("*******")
             var demo1 = moment().dateRangeToDates({
-                rangeStart: startDate,
+                rangeStart: finalStartDate,
                 rangeEnd: finalEndDate,
                 weekdays: [1, 2, 3, 4, 5]
             });
             console.log("COUNT : ", demo1.length);
-            
-            demo1.forEach(function(value) {
+
+            demo1.forEach(function (value) {
                 console.log(moment(value).format('YYYY-MM-DD'));
                 datesList.push(moment(value).format('YYYY-MM-DD'));
             });
@@ -132,13 +133,13 @@ router.get('/generate', (req, res, next) => {
         if (dayLimit == 25) {
 
             var demo1 = moment().dateRangeToDates({
-                rangeStart: startDate,
+                rangeStart: finalStartDate,
                 rangeEnd: finalEndDate,
                 weekdays: [1, 2, 3, 4, 5, 6]
             });
             console.log("COUNT : ", demo1.length);
 
-            demo1.forEach(function(value) {
+            demo1.forEach(function (value) {
                 console.log(moment(value).format('YYYY-MM-DD'));
                 datesList.push(moment(value).format('YYYY-MM-DD'));
             });
@@ -146,25 +147,74 @@ router.get('/generate', (req, res, next) => {
         if (dayLimit == 30) {
 
             var demo1 = moment().dateRangeToDates({
-                rangeStart: startDate,
+                rangeStart: finalStartDate,
                 rangeEnd: finalEndDate,
                 weekdays: [0, 1, 2, 3, 4, 5, 6]
             });
             console.log("COUNT : ", demo1.length);
 
-            demo1.forEach(function(value) {
+            demo1.forEach(function (value) {
                 console.log(moment(value).format('YYYY-MM-DD'));
                 datesList.push(moment(value).format('YYYY-MM-DD'));
             });
         }
     }
 
+    // Weekly Option, Calculate dates list with start date and end date.
+    if (durationType == "Weeks") {
+        console.log("WEEKLY");
+        const start = moment(finalStartDate).startOf('day');
+        const end = moment(finalEndDate).startOf('day');
+        let current = start;
+        const holidayList = [] // Need to add all the holidays here.
+        while (current.isSameOrBefore(end)) {
+            let nextWeek = moment(current).add(7, 'days');
+            //check if holidays are between nextWeek and current
+            //get number of holidays
+            const number_of_holidays = holidayList.reduce((p, c) => {
+                if (moment(c).isAfter(current) && moment(c).isSameOrBefore(nextWeek)) {
+                    return p + 1;
+                }
+                return p;
+            }, 0)
+            nextWeek = nextWeek.add(number_of_holidays, 'days')
+            // console.log(nextWeek.toDate()+"--");
+            console.log(moment(nextWeek.toDate()).format('YYYY-MM-DD'));
+            datesList.push(moment(nextWeek.toDate()).format('YYYY-MM-DD'));
+            current = nextWeek;
+        }
+
+        datesList.pop();
+        datesList.unshift(finalStartDate);
+        console.log(datesList);
+
+    }
+
+    // Monthly Option, Calculate dates list with start date and end date.
+    if (durationType == "Months") {
+
+        testArray = dateOffsetByMonths(parseInt(duration), finalStartDate.toString(), 'YYYY-MM-DD');
+        testArray.pop();
+        datesList = testArray;
+
+    }
+
+    // This function is used to get all the list of given months according to the start , end dates.
+    function dateOffsetByMonths(months, dateStr, format) {
+        var startDate = moment(dateStr, format);
+        return Array.from(Array(months + 1).keys()).reduce(function (res, n, i) {
+            var date = startDate.clone();
+            date.add(i, 'months');
+            res.push(date.format('YYYY-MM-DD'));
+            return res;
+        }, []);
+    };
+
     res.json({
-        'sesion': 'chanaka from Germany',
         'start': startDate,
         'end': finalEndDate,
         'today': moment().format('MMMM Do YYYY, h:mm:ss a'),
-        'datesList' : datesList
+        'datesList': datesList
     });
 });
 
